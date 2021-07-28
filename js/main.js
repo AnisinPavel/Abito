4955404450e21a46832feff8b52e6b0d15810d4b
AnisinPavel <70282977+AnisinPavel@users.noreply.github.com>
2021-07-28 12:21:03 +0300

    Сверстали страницу товара

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
       var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 5,
-        slidesPerView: 5,
+        spaceBetween: 6,
+        slidesPerView: 6,
         freeMode: true,
         watchSlidesVisibility: true,
         watchSlidesProgress: true,
 
       });
       var swiper2 = new Swiper(".mySwiper2", {
-        spaceBetween: 5,
+        spaceBetween: 6,
                 loop: true,
         thumbs: {
           swiper: swiper,
